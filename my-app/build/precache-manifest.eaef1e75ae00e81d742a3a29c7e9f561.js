46c8c23b84aefaac96100521faf7661d965f1810
hannguyenn <han.nguyen@ufl.edu>
2020-04-04 23:40:34 +0000

    done

---
+++ my-app/build/precache-manifest.eaef1e75ae00e81d742a3a29c7e9f561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce86ba6441f61326d8313726af6ef08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df813c364650f1e119e4",
+    "url": "/static/css/main.de2aae39.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df813c364650f1e119e4",
+    "url": "/static/js/main.4f4a69a4.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
